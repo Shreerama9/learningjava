9a471fce037143a10a39dc4229e834b0d08c3fa3
Shreerama <srssbvs007@gmail.com>
2022-09-01 15:57:35 +0530

    cat

---
+++ Basics/MealCost.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MealCost{
+
+	public static void TotalMealCost(double tiprate, double taxrate, double ListedMealPrice){
+		double tip = tiprate * ListedMealPrice;
+		double tax = taxrate * ListedMealPrice;
+		
+		double result = tip + tax + ListedMealPrice;
+		System.out.println("Total Tip should be payed : " + result);
+
+	}
+public static void main(String[] args){
+		TotalMealCost(.2 , .5, 500);
+
+	}	
+}
