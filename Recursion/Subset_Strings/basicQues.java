a7201196ebee51fadc2607f5f03311c13504d072
Shreerama <srssbvs007@gmail.com>
2022-09-17 22:09:59 +0530

    Subset

--- Recursion/Subset_Strings/basicQues.java
+++ Recursion/Subset_Strings/basicQues.java
@@ -2,13 +2,42 @@ package Recursion.Subset_Strings;
 
 public class basicQues {
     public static void main(String[] args) {
-        String str = "baccaf";
+        String str1 = "baccaf";
+        remova("", str1);
+
+        String str2 = "Violetevergarden";
+        System.out.println(removeEver(str2));
+        ;
+
     }
 
-    static void NumOfAlpha(String str, char target) {
-        if (str.length() == 0) {
+    // Removing char
+    static void remova(String answer, String unprocessed) {
+        if (unprocessed.length() == 0) {
+            System.out.println(answer);
             return;
         }
 
+        char ch = unprocessed.charAt(0);
+
+        if (ch == 'a') {
+            remova(answer, unprocessed.substring(1));
+        } else {
+            remova(answer + ch, unprocessed.substring(1));
+        }
     }
+
+    static String removeEver(String str) {
+        if (str.length() == 0) {
+            return "";
+        }
+
+        if (str.startsWith("ever")) {
+            return removeEver(str.substring(4));
+        } else {
+            return str.charAt(0) + removeEver(str.substring(1));
+        }
+
+    }
+
 }
