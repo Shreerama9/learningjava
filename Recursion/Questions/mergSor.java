fdede9e2a32420dc2940fbf297f4cfc3b983601e
Shreerama <srssbvs007@gmail.com>
2022-10-03 20:09:38 +0530

    .

--- Recursion/Questions/mergSor.java
+++ Recursion/Questions/mergSor.java
@@ -1,4 +1,4 @@
-package Recursion;
+package Recursion.Questions;
 
 import java.util.Arrays;
 
--- Recursion/Subset_Strings/Subset.java
+++ Recursion/Subset_Strings/Subset.java
@@ -1,6 +1,7 @@
 package Recursion.Subset_Strings;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Subset {
@@ -10,6 +11,14 @@ public class Subset {
         for (List<Integer> list : ans) {
             System.out.println(list);
         }
+        System.out.println("********************");
+        // Duplicate integer
+        int[] arrD = { 1, 2, 1 };
+        List<List<Integer>> ansD = subSetDupli(arrD);
+        for (List<Integer> listD : ansD) {
+            System.out.println(listD);
+        }
+
     }
 
     static List<List<Integer>> subSet(int[] arr) {
@@ -25,6 +34,30 @@ public class Subset {
             }
         }
 
+        return outer;
+    }
+
+    static List<List<Integer>> subSetDupli(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        int start = 0;
+        int end = 0;
+        for (int j = 0; j < arr.length; j++) {
+            start = 0;
+            if (j > 0 && arr[j] == arr[j - 1]) {
+                start = end + 1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(arr[j]);
+                outer.add(internal);
+
+            }
+        }
         return outer;
     }
 
